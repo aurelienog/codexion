5de449a46f5555f6e158b68b19a6f786cec4f9fc
Aurelie Nogueira <aurelienog@gmail.com>
2026-06-16 19:12:16 +0200

    added coder_init.c

---
+++ src/coder/coder_init.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   coder_init.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aunoguei <aunoguei@student.42urduliz.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/16 10:46:04 by aunoguei          #+#    #+#             */
+/*   Updated: 2026/06/16 14:51:19 by aunoguei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "codexion.h"
+
+void	init_coders(t_simulation *simulation)
+{
+	size_t		i;
+	size_t		coders_number;
+	t_dongle	*left_dongle;
+	t_dongle	*right_dongle;
+
+	i = 0;
+	coders_number = simulation->config.number_of_coders;
+	while (i < coders_number)
+	{
+		left_dongle = &simulation->dongles[i];
+		if (i == coders_number - 1)
+			right_dongle = &simulation->dongles[0];
+		else
+			right_dongle = &simulation->dongles[i + 1];
+		init_coder(left_dongle, right_dongle, simulation, i);
+		i += 1;
+	}
+}
+
+void	init_coder(t_dongle *left_dongle,
+		t_dongle *right_dongle,
+		t_simulation *simulation,
+		size_t i)
+{
+	t_coder	*coder;
+
+	coder = &simulation->coders[i];
+	coder->id = i + 1;
+	coder->compiles_count = 0;
+	coder->last_compile_start = simulation->start_time;
+	coder->finished = 0;
+	coder->simulation = simulation;
+	coder->left = left_dongle;
+	coder->right = right_dongle;
+}
