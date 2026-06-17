7b4e5d53cce54e4deffa34366dbf3d3ad046da09
Aurelie Nogueira <aurelienog@gmail.com>
2026-06-17 16:52:47 +0200

    added threads

--- src/main.c
+++ src/main.c
@@ -12,18 +12,92 @@
 
 #include "codexion.h"
 
-t_error	run_app(t_simulation *sim)
+
+t_error join_threads(t_monitor *monitor)
+{
+	size_t	i;
+	t_error	error;
+	t_simulation	*simulation;
+
+	i = 0;
+	error = ERROR_NONE;
+	simulation = monitor->simulation;
+	if (pthread_join(monitor->thread, NULL) != 0)
+		error = ERROR_THREAD;
+	while (i < simulation->created_coders)
+	{
+		if (pthread_join(simulation->coders[i].thread, NULL) != 0)
+			error = ERROR_THREAD;
+		i++;
+	}
+	return (error);
+}
+
+void* watch(void *arg)
+{
+	t_monitor	*monitor;
+
+	monitor = (t_monitor*)arg;
+	while (!simulation_finished(monitor->simulation))
+	{
+		check_burnout(monitor->simulation);
+		check_completion(monitor->simulation);
+		usleep(500);
+	}
+	return (NULL);
+}
+
+t_error start_coders(t_simulation *simulation)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < simulation->config.number_of_coders)
+	{
+		if (pthread_create(&simulation->coders[i].thread, NULL, &routine, &simulation->coders[i]) != 0)
+		{
+			pthread_mutex_lock(&simulation->simulation_mutex);
+			simulation->termination_flag = 1;
+			pthread_mutex_unlock(&simulation->simulation_mutex);
+			while (i > 0)
+				pthread_join(simulation->coders[--i].thread, NULL);	
+			return (ERROR_THREAD);
+		}
+		simulation->created_coders++;
+		i++;
+	}
+	return (ERROR_NONE);
+}
+
+t_error start_monitor(t_monitor *monitor)
+{
+	if (pthread_create(&monitor->thread, NULL, &watch, (void *)monitor) != 0)
+			return (ERROR_THREAD);
+	return (ERROR_NONE);
+}
+
+t_error	run_app(t_simulation *simulation)
 {
 	t_error	error;
+	t_monitor	monitor;
+	size_t	i;
 
-	error = start_coders(sim);
+	monitor.simulation = simulation;
+	error = start_coders(simulation);
 	if (error != ERROR_NONE)
 		return (error);
-	error = start_monitor(sim);
+	error = start_monitor(&monitor);
 	if (error != ERROR_NONE)
+	{
+		pthread_mutex_lock(&simulation->simulation_mutex);
+		simulation->termination_flag = 1;
+		pthread_mutex_unlock(&simulation->simulation_mutex);
+		i = simulation->created_coders;
+		while (i > 0)
+			pthread_join(simulation->coders[--i].thread, NULL);
 		return (error);
-	join_threads(sim);
-	return (ERROR_NONE);
+	}
+	return (join_threads(&monitor));
 }
 
 int	main(int argc, char **argv)
@@ -46,3 +120,4 @@ int	main(int argc, char **argv)
 	destroy_simulation(simulation);
 	return (error != ERROR_NONE);
 }
+// pthread_cond_broadcast(...)
