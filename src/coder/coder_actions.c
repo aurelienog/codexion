68bb73e8239f5a25c74a59b1be0d2d98195b59f5
Aurelie Nogueira <aurelienog@gmail.com>
2026-06-22 19:14:47 +0200

    added request_compile action

--- src/coder/coder_actions.c
+++ src/coder/coder_actions.c
@@ -6,81 +6,53 @@
 /*   By: aunoguei <aunoguei@student.42urduliz.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 10:51:20 by aunoguei          #+#    #+#             */
-/*   Updated: 2026/06/16 10:51:23 by aunoguei         ###   ########.fr       */
+/*   Updated: 2026/06/22 15:55:32 by aunoguei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
-    // bool    is_compiling;
-    // bool    is_refactoring;
-    // bool    is_debugging;
-    //request_dongle()
-    // compile():
-        // is_compiling = True
-        //
-        //replace_dongles()
-        //is_compiling = False
-        // debug()
-
-    // debug():
-        //is_debugging = True
-        //
-        //is_debugging = False
-        //refactor()
-
-
-    // refactor():
-        //is_refactoring = True
-        //
-        //is_refactoring = False
-void    request_compile(t_coder *coder)
+t_request	create_request(t_coder *coder)
 {
-    int is_left_available;
-    int is_right_available;
-    t_dongle    *left_dongle;
-    t_dongle    *right_dongle;
+	t_request		request;
+	t_simulation	*simulation;
 
-    left_dongle = coder->left;
-    right_dongle = coder->right;
-    is_left_available = 0;
-    is_right_available = 0;
-    if (pthread_mutex_lock(&left_dongle->mutex) != 0)
-        return (ERROR_MUTEX);
-    if (pthread_mutex_lock(&right_dongle->mutex) != 0)
-    {
-        pthread_mutex_unlock(&left_dongle->mutex)
-        return (ERROR_MUTEX);
-    }
-
-    if (left_dongle->is_available && is_next(left_dongle, coder))
-        is_left_available = 1;
-
-    if (right_dongle->is_available && is_next(right_dongle, coder))
-        is_right_available = 1;
-
-    if (is_left_available && is_right_available)
-    {
-        if (pthread_mutex_lock(coder->simulation) != 0)
-        {
-                pthread_mutex_unlock(&left_dongle->mutex);
-                pthread_mutex_unlock(&right_dongle->mutex->mutex);
-                return ;
-        }
-        take_dongles();
-        left->is_available = 0;
-        right->is_available = 0;
-    }
-    else
-    {
-        enqueue_request(t_dongle *dongle, t_request request);
-        wait();
-    }
-    pthread_mutex_unlock(&left_dongle->mutex);
-    pthread_mutex_unlock(&right_dongle->mutex);
-    pthread_mutex_unlock(&coder->simulation->mutex);
+	simulation = coder->simulation;
+	request.coder = coder;
+	request.arrival_time = get_time_ms();
+	pthread_mutex_lock(&simulation->state_mutex);
+	request.order = simulation->request_counter++;
+	pthread_mutex_unlock(&simulation->state_mutex);
+	request.deadline = (coder->last_compile_start
+			+ simulation->config.time_to_burnout);
+	return (request);
 }
-    
-    release_dongles();
-    debug()
-    refactor()
+
+void	request_compile(t_coder *coder)
+{
+	t_request	request;
+
+	request = create_request(coder);
+	enqueue_request(coder->left, &request);
+	enqueue_request(coder->right, &request);
+	while (!can_compile(coder) && !simulation_finished())
+		pthread_cond_wait(&coder->simulation->scheduler.cond,
+				&coder->simulation->scheduler.mutex);
+scheduler_mutex
+	//take_dongles;
+	dequeue_request(coder->left, &request);
+	dequeue_request(coder->right, &request);
+}
+
+void	release_dongles(t_coder *coder)
+{
+	coder->left->is_available = 1;
+	coder->right->is_available = 1;
+	pthread_mutex_lock(&coder->simulation->scheduler.mutex);
+	pthread_cond_broadcast(&coder->simulation->scheduler.cond);
+	pthread_mutex_unlock(&coder->simulation->scheduler.mutex);
+}
+//compile()
+
+// debug()
+// refactor()
--- src/coder/coder_routine.c
+++ src/coder/coder_routine.c
@@ -6,29 +6,26 @@
 /*   By: aunoguei <aunoguei@student.42urduliz.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 10:51:39 by aunoguei          #+#    #+#             */
-/*   Updated: 2026/06/16 10:51:41 by aunoguei         ###   ########.fr       */
+/*   Updated: 2026/06/22 10:30:24 by aunoguei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
-void* routine(void *arg)
+void	*routine(void *arg)
 {
-    t_coder *coder;
-    coder = (t_coder *)arg;
-    while (!simulation_finished(coder->simulation)
-        && coder->compiles_count < coder->simulation->config.number_of_compiles_required)
-    {
-        request_left_dongle();
-        request_right_dongle();
+	t_coder	*coder;
+	int		required_compiles;
 
-        compile();
-
-        release_left();
-        release_right();
-
-        debug();
-
-        refactor();
-    }
+	required_compiles = coder->simulation->config.number_of_compiles_required;
+	coder = (t_coder *)arg;
+	while (!simulation_finished(coder->simulation)
+		&& coder->compiles_count < required_compiles)
+	{
+		request_compile(coder);
+		compile();
+		release_dongles();
+		debug();
+		refactor();
+	}
 }
