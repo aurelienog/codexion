6957ed7a1c9a49a1b77fe9c53d3a46edf278651b
Aurelie Nogueira <aurelienog@gmail.com>
2026-06-22 19:16:40 +0200

    refactor: using scheduler

--- src/dongle/dongle.c
+++ src/dongle/dongle.c
@@ -6,7 +6,7 @@
 /*   By: aunoguei <aunoguei@student.42urduliz.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 11:02:17 by aunoguei          #+#    #+#             */
-/*   Updated: 2026/06/16 15:53:30 by aunoguei         ###   ########.fr       */
+/*   Updated: 2026/06/22 15:59:55 by aunoguei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ t_error	init_dongle(t_dongle *dongle, t_simulation *simulation)
 	dongle->queue.capacity = 0;
 	if (pthread_mutex_init(&dongle->mutex, NULL) != 0)
 		return (ERROR_MUTEX);
-	if (pthread_cond_init(&dongle->cond, NULL) != 0)
-	{
-		pthread_mutex_destroy(&dongle->mutex);
-		return (ERROR_MUTEX);
-	}
+	// if (pthread_cond_init(&dongle->cond, NULL) != 0)
+	// {
+	// 	pthread_mutex_destroy(&dongle->mutex);
+	// 	return (ERROR_MUTEX);
+	// }
 	return (ERROR_NONE);
 }
 
@@ -40,13 +40,13 @@ t_error	init_dongles(t_simulation *simulation)
 		error = init_dongle(&simulation->dongles[i], simulation);
 		if (error != ERROR_NONE)
 		{
-			while (i > 0)
+/*			while (i > 0)
 			{
 				i--;
-				pthread_cond_destroy(&simulation->dongles[i].cond);
-				pthread_mutex_destroy(&simulation->dongles[i].mutex);
+				// pthread_cond_destroy(&simulation->dongles[i].cond);
+				//pthread_mutex_destroy(&simulation->dongles[i].mutex);
 			}
-			return (error);
+*/			return (error);
 		}
 		i++;
 	}
--- src/dongle/dongle_acquire.c
+++ src/dongle/dongle_acquire.c
@@ -1,24 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dongle_acquire.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aunoguei <aunoguei@student.42urduliz.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/22 10:37:55 by aunoguei          #+#    #+#             */
+/*   Updated: 2026/06/22 15:56:50 by aunoguei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "codexion.h"
 
-request_compile()
-
-int can_compile(t_request *req)
-{
-    return (
-        req->coder->left->is_available
-        && req->coder->right->is_available
-        && cooldown_finished(req->coder->left)
-        && cooldown_finished(req->coder->right)
-        && coder == coder
-    );
-}
-
-int	is_next(t_dongle *dongle, t_coder *coder)
-{
-	t_request	*top;
-
-	top = heap_peek(&dongle->queue);
-	if (!top)
-		return (0);
-	return (top->coder == coder);
-}
+//request_compile()
+// int can_compile(t_request *req)
+// {
+//     return (
+//         req->coder->left->is_available
+//             && req->coder->right->is_available
+//             && cooldown_finished(req->coder->left)
+//             && cooldown_finished(req->coder->right)
+//             && coder == coder
+//     );
+// }
