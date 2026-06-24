1a548e2de69d3f5a1a9a8691070ab96f91b6522b
Aurelie Nogueira <aurelienog@gmail.com>
2026-06-24 19:11:38 +0200

    fix: dongle release_time initialised at 0

--- src/dongle/dongle.c
+++ src/dongle/dongle.c
@@ -6,18 +6,18 @@
 /*   By: aunoguei <aunoguei@student.42urduliz.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 11:02:17 by aunoguei          #+#    #+#             */
-/*   Updated: 2026/06/22 15:59:55 by aunoguei         ###   ########.fr       */
+/*   Updated: 2026/06/24 13:29:17 by aunoguei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
-static t_error	init_dongle(t_dongle *dongle, t_simulation *simulation)
+static t_error	init_dongle(t_dongle *dongle)
 {
 	if (pthread_mutex_init(&dongle->mutex, NULL) != 0)
 		return (ERROR_MUTEX);
 	dongle->is_available = 1;
-	dongle->release_time = simulation->start_time;
+	dongle->release_time = 0;
 	dongle->queue.data = NULL;
 	dongle->queue.size = 0;
 	dongle->queue.capacity = 0;
@@ -26,13 +26,13 @@ static t_error	init_dongle(t_dongle *dongle, t_simulation *simulation)
 
 t_error	init_dongles(t_simulation *simulation)
 {
-	size_t	i;
+	int		i;
 	t_error	error;
 
 	i = 0;
 	while (i < simulation->config.number_of_coders)
 	{
-		error = init_dongle(&simulation->dongles[i], simulation);
+		error = init_dongle(&simulation->dongles[i]);
 		if (error != ERROR_NONE)
 		{
 			while (i > 0)
